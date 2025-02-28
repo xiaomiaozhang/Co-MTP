f165c71dce8effc3cd2acd3c0b6d507cc8f68ce7
xiaomiaozhang <148336749+xiaomiaozhang@users.noreply.github.com>
2025-02-28 13:30:39 +0800

    Update maps_merge.py

--- preprocess/maps_merge.py
+++ preprocess/maps_merge.py
@@ -17,7 +17,6 @@ def merge_map_json(maps):
 
     id_num = 0
 
-#    for map in tqdm(maps, desc='merge_map_json'):
     for map in maps:
         
         with open(os.path.join(map_dir, map), encoding='utf-8') as f:
@@ -102,60 +101,12 @@ def merge_map_json(maps):
     print('Completed!!!')
 
 
-# def merge_halluc(maps):
-#
-#     idx = 0
-#     map_dict = {}
-#     polygon_ls = []
-#
-#     lane_dict = {}
-#     for map in maps:
-#         with open(os.path.join(map_dir, map), encoding='utf-8') as f :
-#             for lane_id, lane in json.load(f)['LANE'].items():
-#                 lane_dict[lane_id] = lane
-#
-#     for lane_id, lane in tqdm(lane_dict.items(), desc='merge_halluc'):
-#         map_dict[str(idx)] = lane_id
-#
-#         lane_ls = []
-#
-#         for pt in lane['centerline']:
-#             xy = []
-#             xy.append(float(pt[1:14]))
-#             xy.append(float(pt[-15:-1]))
-#             lane_ls.append(xy)
-#
-#         polygon = centerline_to_polygon(np.array(lane_ls))
-#
-#         x_min, y_min = 500000, 5000000
-#         x_max, y_max = 0, 0
-#
-#         for iter in polygon:
-#
-#             x_min = min(x_min, iter[0])
-#             x_max = max(x_max, iter[0])
-#             y_min = min(y_min, iter[1])
-#             y_max = max(y_max, iter[1])
-#
-#         polygon_xy = [x_min, y_min, x_max, y_max]
-#         polygon_ls.append(polygon_xy)
-#
-#         idx += 1
-#
-#     polygon_ls = np.array(polygon_ls)
-#
-#     with open(os.path.join(output_dir, 'yizhuang_PEK_tableidx_to_laneid_map.json'),"w") as f:
-#         json.dump(map_dict,f)
-#
-#     np.save(os.path.join(output_dir, 'yizhuang_PEK_halluc_bbox_table.npy'), polygon_ls)
-
 def merge_multiple_maps():
 
     maps = os.listdir(map_dir)
 
     merge_map_json(maps)
 
-#    merge_halluc(maps)
 
 def parse_args():
     """Parse input arguments."""
